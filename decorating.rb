27c6bec2f0baa9b97bb1ccd2dab755a898d8c9e5
ActiveApps <douglas.luzinda.ot@gmail.com>
2023-09-10 14:40:43 +0300

    fix errors

--- decorating.rb
+++ decorating.rb
@@ -1,31 +1,24 @@
-# Represents a nameable entity.
 class Nameable
   def correct_name
-    raise NotImplementedError, "Subclasses must implement the 'correct_name' method"
+    raise NotImplementedError
   end
 end
-
-# Represents a decorator for a Nameable object.
 class Decorator < Nameable
   def initialize(nameable)
     @nameable = nameable
+    super()
   end
-
   def correct_name
     @nameable.correct_name
   end
 end
-
-# Represents a decorator that capitalizes the output of a Nameable object's correct_name method.
 class CapitalizeDecorator < Decorator
   def correct_name
-    super.capitalize
+    @nameable.correct_name.capitalize
   end
 end
-
-# Represents a decorator that trims the output of a Nameable object's correct_name method to a maximum of 10 characters.
 class TrimmerDecorator < Decorator
   def correct_name
-    super[0..9]
+    @nameable.correct_name[0, 9]
   end
 end
--- person.rb
+++ person.rb
@@ -1,42 +1,28 @@
-
-# Represents a nameable entity.
-class Nameable
-  def correct_name
-    raise NotImplementedError, "Subclasses must implement the 'correct_name' method"
-  end
-end
-
+require './decorating'
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
-
-  def initialize(age, parent_permission: true, name: "Unknown")
-    super()
-    @id = generate_id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
-
   def can_use_services?
-    of_age? || @parent_permission
+    of_age? || parent_permission
   end
-
   def correct_name
-    puts name
-    name
+    @name
   end
-
   private
-
   def of_age?
     @age >= 18
   end
-
-  def generate_id
-    # Generate and return an ID here
-  end
 end
-
-person = Person.new(12)
+person = Person.new(22, 'maximilianus')
 person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name    
